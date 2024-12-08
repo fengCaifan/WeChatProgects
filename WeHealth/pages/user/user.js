f57c016ba4a2003c8212bfe9e1f5ebe10d1518b2
fengcaifan <799849459@qq.com>
2024-12-08 17:17:49 +0800

    WeHealth个人主页第一版

--- WeHealth/pages/user/user.js
+++ WeHealth/pages/user/user.js
@@ -5,7 +5,12 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    menuItems: [
+      {icon: "/assets/desc_test.png", title: "我的资料"},
+      {icon: "/assets/desc_test.png", title: "团队患者"},
+      {icon: "/assets/desc_test.png", title: "生成直播海报"},
+      {icon: "/assets/desc_test.png", title: "线索收集"}
+    ],
   },
 
   /**
@@ -15,52 +20,9 @@ Page({
 
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+  clickUserHeader() {
+    wx.showModal({
+      title: '点击了头像昵称'
+    })
   }
 })
